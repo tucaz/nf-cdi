72705d6dc731a0ff928db1b5ff940211edae4632
tucaz <me@tucaz.net>
2019-12-01 20:58:10 -0600

    Fixing order of XML element

--- src/NF/NotaFiscal/EnvLoteRPS.cs
+++ src/NF/NotaFiscal/EnvLoteRPS.cs
@@ -109,10 +109,10 @@ namespace NF.NotaFiscal
     [XmlRoot(ElementName = "Contato", Namespace = "http://www.ginfes.com.br/tipos_v03.xsd")]
     public class Contato
     {
-        [XmlElement(ElementName = "Email", Namespace = "http://www.ginfes.com.br/tipos_v03.xsd")]
-        public string Email { get; set; }
         [XmlElement(ElementName = "Telefone", Namespace = "http://www.ginfes.com.br/tipos_v03.xsd")]
         public string Telefone { get; set; }
+        [XmlElement(ElementName = "Email", Namespace = "http://www.ginfes.com.br/tipos_v03.xsd")]
+        public string Email { get; set; }
     }
 
     [XmlRoot(ElementName = "Endereco", Namespace = "http://www.ginfes.com.br/tipos_v03.xsd")]
